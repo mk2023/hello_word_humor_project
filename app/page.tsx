3b08ffd13cac3b1dfcecd00faaa4251863a37d02
Minseul Kim <minseul@beninc.ai>
2026-01-28 16:46:27 -0500

    Added Hello World Text

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+export default function Home() {
+  return (
+    <main>
+        <h1>Hello World!</h1>
+        <p>Built using Next.js and deployed on Vercel.</p>
+     </main>
+  );
+}
