1d2d1808d35892f2217d939cc1df130bb2d554c6
Minseul Kim <minseul@beninc.ai>
2026-02-02 13:36:41 -0500

    Connected and reading from Supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
 import Image from "next/image";
+import { createSupabaseClient } from "@/lib/supabase/supabaseServer";
+import { Suspense } from "react";
+
+async function CaptionExamplesData() {
+  const supabase = await createSupabaseClient();
+  const { data: caption_examples, error } = await supabase.from("caption_examples").select().order("created_datetime_utc", {ascending: false});
+
+  if (error){
+    return <p> Caption Loading Error </p>;
+  }
+  return (
+    <ul>
+        {caption_examples.map((row)=>(
+            <li key = {row.id} style = {{marginBottom: "0.75rem", marginLeft: "0.85rem"}}>
+                {row.caption}
+            </li>
+        ))}
+    </ul>
+  );
+}
+
 export default function Home() {
   return (
     <main>
-        <h1>Hello World!</h1>
-        <p>Built using Next.js and deployed on Vercel.</p>
-     </main>
+        <h1 style = {{fontSize: "3rem"}}>Hello!</h1>
+         <h1 style = {{fontSize: "3rem"}}>Printing all the caption examples ordered by the date they were made! (newest to oldest) </h1>
+         <Suspense fallback={<div>Loading caption_examples...</div>}>
+           <CaptionExamplesData />
+         </Suspense>
+    </main>
   );
 }
